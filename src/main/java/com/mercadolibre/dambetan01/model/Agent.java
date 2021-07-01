9293a3da8ec120a09f7313c33f6e9a0b2d189b4e
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2021-07-01 16:28:39 -0300

    remove getter and setter and use @data

--- src/main/java/com/mercadolibre/dambetan01/model/Agent.java
+++ src/main/java/com/mercadolibre/dambetan01/model/Agent.java
@@ -1,9 +1,16 @@
 package com.mercadolibre.dambetan01.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Agent extends User{
 
     public Agent(Long id, String name, String email, String password, List <UserType> userType) {
--- src/main/java/com/mercadolibre/dambetan01/model/Seller.java
+++ src/main/java/com/mercadolibre/dambetan01/model/Seller.java
@@ -1,17 +1,19 @@
 package com.mercadolibre.dambetan01.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seller extends User{
 
     @Column
     List<String> products;
-
-    public Seller(Long id, String name, String email, String password, List <UserType> userType, List <String> products) {
-        super(id, name, email, password, userType);
-        this.products = products;
-    }
 }
