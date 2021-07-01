7825e3018409d4e20ba5a543e73f108c89aaf6c2
Rogerio Ferreira da Silva <rogerio.silva@mercadolivre.com>
2021-07-01 16:08:32 -0300

    update entity products

--- src/main/java/com/mercadolibre/dambetan01/model/Product.java
+++ src/main/java/com/mercadolibre/dambetan01/model/Product.java
@@ -18,17 +18,6 @@ public class Product {
     private Long id;
 
     private String productName;
-    private String manufacturer;
-    private String composition;
     private String category;
-    private Double weight;
-    private Double price;
-    private String status;
-    private LocalDate manufacturingDate;
-    private LocalDateTime manufacturingTime;
-    private Double minimumTemp;
-    private Double maximumTemp;
-    private LocalDate dueDate;
-
 
 }
