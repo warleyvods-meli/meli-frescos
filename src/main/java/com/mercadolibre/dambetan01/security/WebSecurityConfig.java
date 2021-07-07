a34006199cc84b8e3703843e76ffef56dd0a0296
Warley Vinicius <warleyvods@gmail.com>
2021-07-07 16:00:42 -0300

    jwt funcionando 100%

--- src/main/java/com/mercadolibre/dambetan01/security/WebSecurityConfig.java
+++ src/main/java/com/mercadolibre/dambetan01/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,15 +29,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, LOGAR).permitAll()
-                .antMatchers("/**/admin/**").hasRole("ADMIN")
-                .antMatchers("/**/agent/**").hasRole("AGENT")
-                .antMatchers("/**/seller/**").hasRole("SELLER")
-                .antMatchers("/**/buyer/**").hasRole("BUYER")
+
+//                .antMatchers("/**/admin/**").hasRole("ADMIN")
+//                .antMatchers("/**/agent/**").hasRole("AGENT")
+//                .antMatchers("/**/seller/**").hasRole("SELLER")
+//                .antMatchers("/**/buyer/**").hasRole("BUYER")
+
+                .antMatchers("/**/admin/**").permitAll()
+                .antMatchers("/**/agent/**").permitAll()
+                .antMatchers("/**/seller/**").permitAll()
+                .antMatchers("/**/buyer/**").permitAll()
+
                 .antMatchers(HttpMethod.GET, "/ping").permitAll()
                 .antMatchers(HttpMethod.GET, "/v3/api-docs").permitAll()
                 .antMatchers(HttpMethod.GET, "/fake").permitAll()
                 .antMatchers("/*/actuator/**").permitAll()
-//                .anyRequest().authenticated()
+                .anyRequest().authenticated() // <- Uncomment pra funcionar
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager(), usuarioRepository))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager(), customUserDetailService));
@@ -46,5 +54,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/**");
+//    }
 
 }
