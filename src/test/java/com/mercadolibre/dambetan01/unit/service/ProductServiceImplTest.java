681b7c318397785833027d7852f523a8ecf28114
mguidolin <Senhadificil1@>
2021-07-08 16:02:36 -0300

    product tests

---
+++ src/test/java/com/mercadolibre/dambetan01/unit/service/ProductServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.mercadolibre.dambetan01.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void getAllProductsResponse() {
+    }
+
+    @Test
+    void findProductsCategory() {
+    }
+
+    @Test
+    void getProductLocation() {
+    }
+}
---
+++ src/test/java/com/mercadolibre/dambetan01/util/BuilderProductServiceTest.java
@@ -0,0 +1,2 @@
+package com.mercadolibre.dambetan01.util;public class BuilderProductServiceTest {
+}
